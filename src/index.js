56a255e9283c039a5a7b811fc61bda1e807f09e5
Talia Grace <taliahaller@Talias-iMac.local>
2018-10-12 14:42:54 -0700

    got skeleton with menu, nav bar, and footer.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import './scss/styles.scss';
-import { pingPong } from './ping-pong.js';
+import './scss/index.scss';
+// import { pingPong } from './ping-pong.js';
 import $ from 'jquery';
 
 
 
 
 $(document).ready(function(){
-  $('#ping-pong-form').submit(function(event){
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
-  });
-});
+
+  // $("#nav-burger-line-container").click(function() {
+
+
+
+  // $(window).scroll(function() {
+      // var iCurScrollPos = $(this).scrollTop();
+
+      $("#nav-burger-line-container").click(function() {
+        $(".nav-toggle").toggleClass("active");
+        $(".nav-logo, .nav-burger").toggleClass("nav-bar-white");
+        $(".nav-toggle").toggleClass("nav-toggle-white");
+        $(".rectangle").toggle();
+        $("#menu").fadeToggle();
+        $("#menu-bg-tilt").toggleClass("slideOutUp");
+        $(".menu-col").toggleClass("fadeOutDown");
+      });
+
+
+      // if (iCurScrollPos > 10) {
+      //   $(".navBar").addClass("scrolled");
+      //   $(".bar").addClass("rectangle animated fadeInDown");
+      // } else if (iCurScrollPos < 10){
+      //   $(".navBar").removeClass("scrolled");
+      //   $(".bar").removeClass("rectangle animated fadeInDown");
+      //   $("#h").show();
+      // }
+      // $("#nav-burger-line-container").click(function() {
+      //   $(".nav-toggle").toggleClass("active");
+      //   $("#menu").fadeToggle();
+      //   $("#menu-bg-tilt").toggleClass("slideOutUp");
+      //   $(".menu-col").toggleClass("fadeOutDown");
+      // });
+      //
+      // if ($("#menu").is('hidden')) {
+      //   $(".navBar").removeClass("scrolled");
+      //   $(".bar").removeClass("rectangle animated fadeInDown");
+      //   $("body").removeClass("noscroll");
+      // } else if (iCurScrollPos > 10) {
+      //   $(".navBar").addClass("scrolled");
+      //   $(".bar").addClass("rectangle animated fadeInDown");
+      // }
+    // });
+
+
+  // Hamburger to X Animation
+
+
+}); //document close

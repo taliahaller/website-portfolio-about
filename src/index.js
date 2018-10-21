817f724f959dbc8e1bd72c5d16dacced72591cca
Talia Grace <taliahaller@Talias-iMac.local>
2018-10-20 17:06:23 -0700

    add successful swiper js. Need to solve image problem... not everything getting loaded.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,8 @@ import './scss/index.scss';
 // import { pingPong } from './ping-pong.js';
 import $ from 'jquery';
 
-
-
-
 $(document).ready(function(){
 
-  // $("#nav-burger-line-container").click(function() {
-
-
-
-  // $(window).scroll(function() {
-      // var iCurScrollPos = $(this).scrollTop();
 
       $("#nav-burger-line-container").click(function() {
         $(".nav-toggle").toggleClass("active");
@@ -24,34 +15,6 @@ $(document).ready(function(){
         $(".menu-col").toggleClass("fadeOutDown");
       });
 
-
-      // if (iCurScrollPos > 10) {
-      //   $(".navBar").addClass("scrolled");
-      //   $(".bar").addClass("rectangle animated fadeInDown");
-      // } else if (iCurScrollPos < 10){
-      //   $(".navBar").removeClass("scrolled");
-      //   $(".bar").removeClass("rectangle animated fadeInDown");
-      //   $("#h").show();
-      // }
-      // $("#nav-burger-line-container").click(function() {
-      //   $(".nav-toggle").toggleClass("active");
-      //   $("#menu").fadeToggle();
-      //   $("#menu-bg-tilt").toggleClass("slideOutUp");
-      //   $(".menu-col").toggleClass("fadeOutDown");
-      // });
-      //
-      // if ($("#menu").is('hidden')) {
-      //   $(".navBar").removeClass("scrolled");
-      //   $(".bar").removeClass("rectangle animated fadeInDown");
-      //   $("body").removeClass("noscroll");
-      // } else if (iCurScrollPos > 10) {
-      //   $(".navBar").addClass("scrolled");
-      //   $(".bar").addClass("rectangle animated fadeInDown");
-      // }
-    // });
-
-
-  // Hamburger to X Animation
 
 
 }); //document close
